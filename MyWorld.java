76f0eda078883a3a88bd746d5535911384683fa3
Carter867 <carterylkwong@gmail.com>
2025-05-21 08:31:12 -0400

    added movement

--- MyWorld.java
+++ MyWorld.java
@@ -5,6 +5,6 @@ public class MyWorld extends World {
         super(600, 400, 1);
         
         FlappyBoy flappy = new FlappyBoy();
-        addObject(flappy, 300, 300);
+        addObject(flappy, 300, 200);
     }
 }
