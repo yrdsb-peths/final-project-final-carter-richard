37bd9a19168af66e3f613bbb8f8cac518d339db6
Carter867 <carterylkwong@gmail.com>
2025-05-21 08:49:16 -0400

    added movement up and down

--- FlappyBoy.java
+++ FlappyBoy.java
@@ -12,16 +12,17 @@ public class FlappyBoy extends Actor
      * Act - do whatever the FlappyBoy wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act()
-    {
-        if(Greenfoot.isKeyDown("up"))
+public void act()
+    {   
+    if(Greenfoot.isKeyDown("up"))
         {
-            move(-1);
+            setLocation(getX(), getY() - 5); 
         }
-        else if(Greenfoot.isKeyDown("down"));
+        else if(Greenfoot.isKeyDown("down"))
         {
-            move(1);
+            setLocation(getX(), getY() + 5); 
         }
 
     }
 }
+
