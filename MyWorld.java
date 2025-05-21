6faaf8b93bded8ffb14f7dde0cc385cf2a9198a7
Carter867 <carterylkwong@gmail.com>
2025-05-21 08:25:27 -0400

    added flappyBoy

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,8 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        FlappyBoy flappy = new FlappyBoy();
+        addObject(flappy, 300, 300);
     }
 }
