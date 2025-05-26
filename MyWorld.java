9b83d1c521a4349f67bbb0d2afd0f59295029358
Carter867 <carterylkwong@gmail.com>
2025-05-26 08:32:14 -0400

    added a scoreboard

--- MyWorld.java
+++ MyWorld.java
@@ -2,28 +2,33 @@ import greenfoot.*;
 
 public class MyWorld extends World {
     int counter = 0;
-    
+    private int score = 0;
+
     public MyWorld() {
         super(850, 478, 1);
-        
+
         FlappyBoy flappy = new FlappyBoy();
-
-        addObject(flappy, 300, 300);
-
         addObject(flappy, 175, 300);
 
-    
+        showScore(); // Show initial score
     }
-    
-    public void act()
-    {
+
+    public void act() {
         counter++;
-        if(counter == 100) 
-        {
+        if(counter == 100) {
             Pipe pipes = new Pipe();
             addObject(pipes, getWidth(), getHeight()/2);
-            System.out.println(counter);
             counter = 0;
         }
     }
+
+    public void increaseScore() {
+        score++;
+        showScore();
+    }
+
+    private void showScore() {
+        showText("Score: " + score, 100, 30);
+    }
 }
+
--- Pipe.java
+++ Pipe.java
@@ -9,8 +9,20 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Pipe extends Actor
 {
     int pipeSpeed = -4;
+    private boolean passed = false;
+
     public void act()
     {
-        setLocation(getX() + pipeSpeed, getY() );
+        setLocation(getX() + pipeSpeed, getY());
+
+        FlappyBoy flappy = (FlappyBoy) getWorld().getObjects(FlappyBoy.class).get(0);
+        if (!passed && getX() + getImage().getWidth() / 2 < flappy.getX()) {
+            passed = true;
+            ((MyWorld) getWorld()).increaseScore();
+        }
+
+        if (getX() < -getImage().getWidth()) {
+            getWorld().removeObject(this);
+        }
     }
 }
